1703df9580b49dfd763067244bda2a3a67595ebf
Bradderz080 <90431934+Bradderz080@users.noreply.github.com>
2021-09-24 12:55:59 +0800

    Update higherlower.py
    Added Partner B version of higherlower game.

--- higherlower.py
+++ higherlower.py
@@ -5,28 +5,39 @@ Purpose: Higher Lower Game
 """
 import random
 
+count = 0
 
-def main():
+
+def main(count):
     try:
         guess = int(input(">"))
-        higher_lower(guess, number)
+        if guess > 100:
+            print("The number is between 0 and 100, try again:")
+            main(count)
+        elif guess < 0:
+            print("The number is between 0 and 100, try again:")
+            main(count)
+        else:
+            count += 1
+            higher_lower(guess, number, count)
     except ValueError:
-        print("ERROR! Try again")
+        print("ERROR! Numbers only! Try again")
+        main(count)
 
 
-def higher_lower(guess, number):
-    if guess == number:
-        print("Congrats")
-        return
-    elif guess > number:
+def higher_lower(guess, number, count):
+    if guess > number:
         print("Your number is too high, guess again")
-        main()
-    else:
+        main(count)
+    elif guess < number:
         print("Your number is too low, guess again")
-        main()
+        main(count)
+    else:
+        print("Congrats! You took " + str(count) + " turns!")
+        return
 
 
 if __name__ == "__main__":
     number = random.randint(0, 100)
     print("Please enter an integer between 0 and 100:")
-    main()
+    main(count)
